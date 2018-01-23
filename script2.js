1e5d774890dfd48c31bbbc4851541710a2cf1307
unknown <jtc7537@gmail.com>
2018-01-22 22:51:54 -0500

    worked on bug in game choice selection

--- script2.js
+++ script2.js
@@ -6,19 +6,8 @@ var config = {
     storageBucket: "rps-multiplayer-b00d5.appspot.com",
     messagingSenderId: "46073764810"
   };
-  firebase.initializeApp(config);
 
-$(".rps-image").click(function(){
-    if(userOneSelected || userTwoSelected){
-        return;
-    }
-    $(this).addClass("clicked-image");
-    console.log()
-})
-
-$("#chat-input").click(function(){
-    $(this).val("")
-})
+firebase.initializeApp(config);
 
 var userOneSelected = false;
 var userTwoSelected = false;
@@ -34,8 +23,6 @@ let newChoice1;
 let newChoice2;
 var userName = prompt("Please enter a username for the in-game chat:")
 
-
-
 gameOver.set({
     gameStatus: false,
 })
@@ -43,7 +30,20 @@ gameOver.set({
 playerOneChoice.remove();
 playerTwoChoice.remove();
 
+$(".rps-image").click(function(){
+    if(userOneSelected){
+        return;
+    }
+    if(userTwoSelected){
+        return;
+    }
+    $(this).addClass("clicked-image");
+    console.log()
+})
 
+$("#chat-input").click(function(){
+    $(this).val("")
+})
 
 gameOver.on("value",
     function(snapshot){
@@ -57,37 +57,63 @@ gameOver.on("value",
         }
     },
     function(errData){
-        console.log("Error retrieving data")
-    })
+    console.log("Error retrieving data")
+    }
+)
+
 playerOneChoice.on("value",
     function(snapshot){
         console.log(snapshot.val())
         if(snapshot.val()){ 
-        console.log("player one selectedd")
-        userOneSelected = true;
-        newChoice1 = snapshot.val().choice
+            console.log("player one selectedd")
+            userOneSelected = true;
+            newChoice1 = snapshot.val().choice
         } else {
             return;
         }
     },
     function(snapshot){
         console.log("error")
-    })
+    }
+)
+
 playerTwoChoice.on("value",
     function(snapshot){
         if(snapshot.val()){
             console.log("recieved user two choice data")
-        newChoice2 = snapshot.val().choice
+            newChoice2 = snapshot.val().choice
+            userTwoSelected = true;
         } else {
             return;
         }
     },
     function(snapshot){
         console.log("error")
-    })
+    }
+)
+
 chat.on("value",gotChatData,errChatData)
 
+function gotChatData(data){
+    var chatLog = data.val();
+    console.log(chatLog)
+    var keys = Object.keys(chatLog)
+    console.log(keys)
+    $("#chat-box").empty();
+    for(var i = 0; i < keys.length; i++){
+        var keyIndex = keys[i];
+        var chatEntry = chatLog[keyIndex].message;
+        console.log(chatEntry)
+        var p = $("<p>")
+        p.text(chatEntry).addClass("chat-entry")
+        $("#chat-box").append(p)
+    }
+}
 
+function errChatData(data){
+    console.log(data);
+    console.log("Error")
+}
 
 $("#chat-input").click(function(){
     $(this).val("")
@@ -116,27 +142,6 @@ function enterChat(){
     }
     chat.push(userInput)
     console.log(userInput)
-}
-
-function gotChatData(data){
-    var chatLog = data.val();
-    console.log(chatLog)
-    var keys = Object.keys(chatLog)
-    console.log(keys)
-    $("#chat-box").empty();
-    for(var i = 0; i < keys.length; i++){
-        var keyIndex = keys[i];
-        var chatEntry = chatLog[keyIndex].message;
-        console.log(chatEntry)
-        var p = $("<p>")
-        p.text(chatEntry).addClass("chat-entry")
-        $("#chat-box").append(p)
-    }
-}
-
-function errChatData(data){
-    console.log(data);
-    console.log("Error")
 }
 
 $("#clear-chat").click(function(){
@@ -207,55 +212,3 @@ $("#clear-chat").click(function(){
       playerOneChoice.remove();
       playerTwoChoice.remove();
   })
-
-
-  //var ref = database.ref('scores')
-  //ref.on("value", gotData, errData)
-  //var score = 0;
-
-//function gotData(data){
-//    $("#score-display").html("");
-//    console.log("got data")
-//    var scores = data.val();
-//    console.log(scores)
-//    var keys = Object.keys(scores);
-//    console.log(keys)
-//    for(var i = 0; i < keys.length; i++){
-//        var keyIndex = keys[i];
-//        var initials = scores[keyIndex].name;
-//        var score = scores[keyIndex].score;
-//        console.log(initials, score)
-//        var p = $("<p>")
-//        p.text(initials + score)
-//        $("#score-display").append(p)
-//    }
-//}
-//
-//function errData(err) {
-//    console.log("error")
-//    console.log(err)
-//}
-//
-//
-//  $("#submit").click(function(){
-//      submitScore()
-//  })
-//
-//
-//  function submitScore() {
-//    var data = {
-//        name: $("#initials").val(),
-//        score: score
-//    }
-//
-//    var ref = database.ref('scores')
-//    console.log(data)
-//    ref.push(data)
-//  }
-//
-//
-//
-//  $("#click").click(function(){
-//    score++;
-//    $("#score").text(score)
-//})
